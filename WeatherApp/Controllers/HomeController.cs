326a6535106c6d6747211363c34af72da9676d20
rcluca <rc_luca@yahoo.com>
2015-10-23 14:27:35 -0400

    Removed Highcharts test page

--- WeatherApp/Controllers/HomeController.cs
+++ WeatherApp/Controllers/HomeController.cs
@@ -66,21 +66,6 @@ namespace WeatherApp.Controllers
             return PartialView(chart);
         }
 
-        public ActionResult ChartTest()
-        {
-            Highcharts chart = new Highcharts("chart")
-                .SetXAxis(new XAxis
-                {
-                    Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
-                })
-                .SetSeries(new Series
-                {
-                    Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
-                });
-
-            return View(chart);
-        }
-
         #endregion
     }
 }
